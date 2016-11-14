6ca366651b5d705a0de151375f4d1e5f25cdf669
Braden Tholkes <tholkesbraden@gmail.com>
2016-11-14 12:08:23 -0500

    Commit Four (I guess)
    Accidentally forgot to change .gitignore for Unity as well as Visual
    Studio, all fixed now.

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -4,18 +4,16 @@ using System.Collections;
 public class Controls : MonoBehaviour
 {
     public Transform groundCheck;       //transform linked to player that tests for ground
-    public Transform wallCheck_R;       //transform linked to player that tests for wall on the right
-    public Transform wallCheck_L;       //transform linked to player that tests for wall on the left
+    public Transform wallCheck;       //transform linked to player that tests for wall on the right
     public LayerMask groundLayer;       //added layer dropdown to set what layer it will test against
 
-    [HideInInspector] public bool grounded = false;     //ground boolean
-    [HideInInspector] public bool walledR = false;      //wall right boolean
-    [HideInInspector] public bool walledL = false;      //wall left boolean
-    [HideInInspector] public bool jump;     //boolean for if the player can jump or not
+    bool grounded = false;     //ground boolean
+    bool touchingWall = false;      //wall right boolean
+    bool doubleJump = false;
 
     public float moveSpeed = 0f;        //variable for speed set within unity for easy change
-    public float jumpSpeed = 0f;        //variable for jump strength set within unity
-    public float maxSpeed = 0f;     //variable for max speed set within unity
+    public float jumpForce = 0f;        //variable for jump strength set within unity
+    public float jumpPushForce = 0f;
 
     private new Rigidbody2D rigidbody2D;        //variable to hold and call the rigidbody
 
@@ -23,32 +21,43 @@ public class Controls : MonoBehaviour
     {
         rigidbody2D = GetComponent<Rigidbody2D>();      //when the program starts, it sets the rigidbody
     }
-
-    void Update()
-    {
-        walledR = Physics2D.OverlapCircle(wallCheck_R.position, 0.05f, groundLayer);        
-        walledL = Physics2D.OverlapCircle(wallCheck_L.position, 0.05f, groundLayer);        /*right and left tests for if the player is up against a wall, sets matching boolean variable accordingly*/
-        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);        //tests if the player is grounded
-
-        if (Input.GetButtonDown("Jump") && (grounded||walledL||walledR))        //if the jump button is pressed, and if the player is grounded or on a wall
-        {
-            jump = true;        //the player is allowed to jump
-        }
-    }
 	
 	void FixedUpdate ()
     {
-        float movex = Input.GetAxis("Horizontal");      //sets movex equal to positive or negative movement (lets it move in the + or - direction, meaning right or left accordingly)
+        touchingWall = Physics2D.OverlapCircle(wallCheck.position, 1f, groundLayer);
 
-        if (movex * rigidbody2D.velocity.x < maxSpeed)      //if the movement speed is under the max allowed speed
-            rigidbody2D.AddForce(Vector2.right * movex * moveSpeed);        //adds to the speed of the rigidbody multiplying movespeed by the current speed by the direction it should apply in, and by movespeed
-        if (Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)       //if the speed of the rigidbody is over the max speed
-            rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);      //set the speed of the rigidbody equal to + or - max speed, and the y velocity
+        grounded = Physics2D.OverlapCircle(groundCheck.position, .5f, groundLayer);        //tests if the player is grounded
 
-        if (jump)       //if the player can jump
+        if (grounded)
+            doubleJump = false;
+
+        if (touchingWall)
         {
-            rigidbody2D.AddForce(new Vector2(0f, jumpSpeed));       //add force to the vector equal to the set jump force
-            jump = false;       //set jump equal to false so it doesn't loop infinitely and jump forever
+            grounded = false;
+            doubleJump = false;
         }
+
+        float move = Input.GetAxis("Horizontal");
+
+        rigidbody2D.velocity = new Vector2(move * moveSpeed, rigidbody2D.velocity.y);
 	}
+
+    void Update()
+    {
+        if ((grounded || !doubleJump) && Input.GetButtonDown("Jump"))
+        {
+            rigidbody2D.velocity = new Vector2(0, jumpForce);
+
+            if (!doubleJump && !grounded)
+                doubleJump = true;
+        }
+
+        if (touchingWall && Input.GetButtonDown("Jump"))
+            wallJump();
+    }
+
+    void wallJump()
+    {
+        rigidbody2D.AddForce(new Vector2(-jumpPushForce, jumpForce));
+    }
 }
